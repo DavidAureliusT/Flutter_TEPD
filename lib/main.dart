1e47d8c09b850ba806ed5cfe4b6e60765a4c909b
daurelius <55678808+daurelius@users.noreply.github.com>
2020-10-11 15:29:17 +0700

    Mission 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/homePage.dart';
 
-Color colorButton = Colors.white;
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -11,111 +11,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Mission 1',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment(0.5, 0.0),
-            colors: [Colors.deepPurpleAccent, Colors.white],
-          ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Image.network(
-                'https://wwwimage-secure.cbsstatic.com/base/files/seo/sbsp_social_1200x627.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.network(
-                      'https://decider.com/wp-content/uploads/2016/07/really-internet-spongebob.jpg?quality=80&strip=all&w=646&h=431&crop=1',
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Spacer(),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.network(
-                      'https://imgix.bustle.com/elite-daily/2015/03/05175344/resting-bitchface.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70',
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Spacer(),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.network(
-                      'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/11/2825613314.png',
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Spacer(),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.network(
-                      'https://ilovespongebobsomuch.weebly.com/uploads/2/0/1/2/20122231/1177880.jpg?327',
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-                flex: 4,
-                child: ListView(
-                  children: [
-                    Text(
-                        'Siapa yang tidak mengenal sosok sponge kuning bercelana kotak satu ini? Dirilis pada 1999, SpongeBob Squarepants menjadi salah satu kartun terpopuler di dunia. Tak hanya dicintai karena keunikan karakter-karakternya, serial animasi karya Stephen Hillenburg ini juga terasa semakin menarik berkat alur cerita yang konyol dan menggelitik. Kisah dalam SpongeBob Squarepants berpusat pada kehidupan makhluk laut di kota Bikini Bottom, nama yang diambil dari bebatuan karang Bikini Atoll di Samudera Pasifik.',
-                        style: TextStyle(fontSize: 18)),
-                    Text(
-                        'SpongeBob Squarepants digambarkan sebagai sosok ceria dan bersahabat. Ia tinggal di sebuah rumah berbentuk nanas dan bekerja sebagai koki di Krusty Krab. Restoran Krusty Krab dimiliki seorang kepiting merah pelit bernama Eugene Krabs. Sebelum menjadi pebisnis, Mr. Krabs merupakan seorang pelaut yang cukup ditakuti.',
-                        style: TextStyle(fontSize: 18)),
-                    Text(
-                        'Selain sibuk membuat Krabby Patty setiap harinya, SpongeBob juga sering disibukkan dengan berbagai petualangan seru bersama sahabatnya, Patrick Star. Kegaduhan yang dibuat SpongeBob dan Patrick sering kali membuat Squidward Tentacles kesal. Ia akan melakukan berbagai cara untuk menghentikan dua tetangga menyebalkan itu.',
-                        style: TextStyle(fontSize: 18))
-                  ],
-                ))
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if (colorButton == Colors.white) {
-              colorButton = Colors.red;
-            } else {
-              colorButton = Colors.white;
-            }
-          });
-        },
-        child: Icon(Icons.favorite),
-        backgroundColor: Colors.lightBlueAccent,
-        foregroundColor: colorButton,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-    ));
+    return MaterialApp(home: HomePage());
   }
 }
